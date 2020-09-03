a782377291cc7d3098cc6344b2b1c4b7dbd17761
lectrel <janne.kovalainen1@gmail.com>
2020-09-03 11:40:12 +0300

    3.11: Add frontend

---
+++ build/precache-manifest.27b1aa60005cc2de5cc13c1ab09e46b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96900e2b68a12b1621522075bb41fdd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f47720bf0f2e0d4131",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "cf5a3232b2c6e8f01fe8",
+    "url": "/static/js/2.3f231bad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f231bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f47720bf0f2e0d4131",
+    "url": "/static/js/main.8d202b04.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);
